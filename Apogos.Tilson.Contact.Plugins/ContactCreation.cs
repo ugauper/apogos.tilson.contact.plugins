b83b2de85322d376ebae07c94f7b43caa75e2654
Una <ugauper@runding.com>
2020-11-13 17:43:14 -0800

    TIL-417 Fixed portal error and adjusted logic for Contacts created through the portal.

--- Apogos.Tilson.Contact.Plugins/ContactCreation.cs
+++ Apogos.Tilson.Contact.Plugins/ContactCreation.cs
@@ -18,6 +18,7 @@ namespace Apogos.Tilson.Contact.Plugins
         private const string _primaryContactIdAttribute = "primarycontactid";
         private const string _parentCustomerIdAttribute = "parentcustomerid";
         private const string _contactTypeAttribute = "apogos_contacttype";
+        private const string _contactTypePortalAttribute = "apogos_contacttypeportal";
         private const int _primaryContactType = 255290001;
         public override List<Models.PluginMessage> SupportedMessages => new List<Models.PluginMessage> { Models.PluginMessage.Create };
 
@@ -40,20 +41,47 @@ namespace Apogos.Tilson.Contact.Plugins
             if (accountReference == null) { return; }
 
             var contactTypes = contact.GetAttributeValue<OptionSetValueCollection>(_contactTypeAttribute);
-            var primaryContactTypeOption = new OptionSetValue(_primaryContactType);
-
-            if (!contactTypes.Contains(primaryContactTypeOption)) { return; }
-            
             var accountId = accountReference.Id;
 
+            if (contactTypes != null)
+            {
+                var primaryContactTypeOption = new OptionSetValue(_primaryContactType);
+
+                if (!contactTypes.Any(c => c.Value == primaryContactTypeOption.Value)) { return; }
+            } else
+            {
+                // This Contact was created through a portal
+                var contactTypePortal = contact.GetAttributeValue<string>(_contactTypePortalAttribute);
+
+                if (!contactTypePortal.Contains(_primaryContactType.ToString())) {
+                    return;
+                }
+
+                var contactTypeOptionSetValues = contactTypePortal.Split(',').Select(contactType => new OptionSetValue(int.Parse(contactType)));
+                var contactTypeValueCollection = new OptionSetValueCollection();
+                contactTypeValueCollection.AddRange(contactTypeOptionSetValues);
+
+                var partialContact = new Models.Contact(new Entity(new Models.Contact().EntityName, contact.Id));
+                partialContact.SetAttribute(_contactTypeAttribute, contactTypeValueCollection);
+
+                partialContact.SetAttribute(_parentCustomerIdAttribute, new EntityReference(new Models.Account().EntityName, accountId));
+
+                var contactService = new ContactService(OrgService, TracingService);
+                contactService.Update(partialContact);
+            }
+
+            var partialAccount = new Models.Account(new Entity(new Models.Account().EntityName, accountId));
+
             var account = (Models.Account)accountService.Get(accountId);
+           
             var existingPrimaryContact = account.GetAttributeValue<EntityReference>(_primaryContactIdAttribute);
 
             if (existingPrimaryContact != null) return;
 
             var contactReference = new EntityReference(new Models.Contact().EntityName, contact.Id);
-            account.SetAttribute(_primaryContactIdAttribute, contactReference);
-            accountService.Update(account);
+           
+            partialAccount.SetAttribute(_primaryContactIdAttribute, contactReference);
+            accountService.Update(partialAccount);
         }
     }
 }
